38bfb588cc5c45b2d478f1d2bda565d76b3e8521
Andrea Roy <akroy2@illinois.edu>
2020-01-09 16:18:06 -0600

    starting alarm system

--- Server.py
+++ Server.py
@@ -31,7 +31,7 @@ def is_unique(reminder):
 '''
 GET Request method to return all notifications on the server as a json 
 '''
-def get_all():
+def get_all_reminders():
     ret = []
     for notification in notification_list:
        ret.append(notification.get_json_dict())  
@@ -44,7 +44,7 @@ GET Request method for an individual reminder
 Takes a title and returns the notification object as a json if it exists
 Otherwise throws a ValueError
 '''
-def get(title):
+def reminder_get(title):
     for notification in notification_list:
         if notification.title == title:
             return notification.get_json()
@@ -58,7 +58,7 @@ POST Request method
 Takes all the requirements to create a new notification object and appends the created notification to notification_list
 Otherwise throws a ValueError
 '''
-def post(title, msg, start, end, rpt):
+def reminder_post(title, msg, start, end, rpt):
     new = Notification.Notification(title, msg, start, end, rpt)
     if is_unique(new) == -1:
         notification_list.append(new)
@@ -75,7 +75,7 @@ Each parameter which isn't passed remains the same on the server
 If the parameter is passed, the field is updated to the parameter
 Updates the notifcation with that title
 '''
-def put(title, msg, start, end, rpt):
+def reminder_put(title, msg, start, end, rpt):
     #if the reminder is not unique throw exception
     if is_unique(Notification.Notification(title, msg, start, end, rpt)) != -1:
         raise ValueError("This event already exists")
@@ -100,7 +100,7 @@ DELETE Request method
 Takes a hashed value representing a Notification and removes it from the server
 If the hashed value isn't found on the server, raise an exception
 '''
-def delete(hash_val):
+def reminder_delete(hash_val):
     for i in range(i, len(notification_list)):
         if hash(notification_list[i]) == hash_val:
             ret = notification_list[i]
@@ -119,9 +119,9 @@ def reminders():
     '''
     if request.method == 'GET':
         if 'title' in request.args:
-            return get(request.args['title'])
+            return reminder_get(request.args['title'])
         else:
-            return get_all()
+            return get_all_reminders()
     #POST
     elif request.method == 'POST':
         if 'title' not in request.args:
@@ -134,19 +134,65 @@ def reminders():
             raise TypeError("No end argument was passed")
         elif 'rpt' not in request.args:
             raise TypeError("No rpt argument was passed")   
-        return post(request.args['title'], request.args['msg'], request.args['start'],
+        return reminder_post(request.args['title'], request.args['msg'], request.args['start'],
                 request.args['end'], request.args['rpt'])
     #PUT
     elif request.method == 'PUT':
         if 'title' not in request.args:
             raise TypeError("No title argument was passed")
-        return put(request.args['title'], request.args['msg'], request.args['start'],
+        return reminder_put(request.args['title'], request.args['msg'], request.args['start'],
                 request.args['end'], request.args['rpt'])
     #DELETE
     elif request.method =='DELETE':
         if 'title' not in request.args:
             raise TypeError("No title argument was passed")
-        return delete(request.args['hash_val'])
+        return reminder_delete(request.args['hash_val'])
+
+
+#define url routing
+@app.route('/alarms', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def alarms():
+    #GET
+    '''
+    if a tle if it exists
+    else get a json with every notification on the server
+    '''
+    if request.method == 'GET':
+        if 'title' in request.args:
+            return reminder_get(request.args['title'])
+        else:
+            return get_all_reminders()
+    #POST
+    elif request.method == 'POST':
+        if 'title' not in request.args:
+            raise TypeError("No title argument was passed")
+        elif 'msg' not in request.args:
+            raise TypeError("No msg argument was passed") 
+        elif 'start' not in request.args:
+            raise TypeError("No start argument was passed")
+        elif 'end' not in request.args:
+            raise TypeError("No end argument was passed")
+        elif 'rpt' not in request.args:
+            raise TypeError("No rpt argument was passed")   
+        return reminder_post(request.args['title'], request.args['msg'], request.args['start'],
+                request.args['end'], request.args['rpt'])
+    #PUT
+    elif request.method == 'PUT':
+        if 'title' not in request.args:
+            raise TypeError("No title argument was passed")
+        return reminder_put(request.args['title'], request.args['msg'], request.args['start'],
+                request.args['end'], request.args['rpt'])
+    #DELETE
+    elif request.method =='DELETE':
+        if 'title' not in request.args:
+            raise TypeError("No title argument was passed")
+        return reminder_delete(request.args['hash_val'])
+
+
+
+
+
+
 
 
 #DRIVER            
